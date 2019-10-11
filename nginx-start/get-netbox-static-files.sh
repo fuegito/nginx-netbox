b6d1d3fa53ead9985e2b609f4a54d08191fd47c1
Stefan Feurle <sfeurle@okd1.mgt.eets.corp>
2019-10-11 14:47:06 +0200

    chown to static files dir

--- nginx-start/get-netbox-static-files.sh
+++ nginx-start/get-netbox-static-files.sh
@@ -9,4 +9,5 @@ static_folder="${app_root}/netbox-static"
 curl https://codeload.github.com/netbox-community/netbox/zip/master > ${repo_zip} 2>/dev/null
 unzip ${repo_zip} >/dev/null 2>&1
 cp -a ${repo_dir}/netbox/project-static ${static_folder}
+chown -R default:root ${static_folder}
 rm -rf ${repo_dir} ${repo_zip}
